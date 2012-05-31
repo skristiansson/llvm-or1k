15d037852bb8fe206cd0dd51e5e6e4a6c6348bf2
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2012-05-31 06:24:14 +0300

    OR1K: promote i1 for extended loads

--- lib/Target/OR1K/OR1KISelLowering.cpp
+++ lib/Target/OR1K/OR1KISelLowering.cpp
@@ -103,6 +103,11 @@ OR1KTargetLowering::OR1KTargetLowering(OR1KTargetMachine &tm) :
   setOperationAction(ISD::SIGN_EXTEND_INREG, MVT::i8,   Expand);
   setOperationAction(ISD::SIGN_EXTEND_INREG, MVT::i16,  Expand);
 
+  // Extended load operations for i1 types must be promoted
+  setLoadExtAction(ISD::EXTLOAD,             MVT::i1,   Promote);
+  setLoadExtAction(ISD::ZEXTLOAD,            MVT::i1,   Promote);
+  setLoadExtAction(ISD::SEXTLOAD,            MVT::i1,   Promote);
+
   setMinFunctionAlignment(4);
   setPrefFunctionAlignment(4);
 }
